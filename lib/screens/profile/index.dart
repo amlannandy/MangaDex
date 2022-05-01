f5bad9ed424641a424e747017238a27462c73517
amlannandy <amlannandy5@gmail.com>
2022-05-01 17:50:51 +0530

    init profile section

--- lib/screens/profile/index.dart
+++ lib/screens/profile/index.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mangadex_mobile/screens/profile/local_widgets/not_authenticated.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profile"),
-    );
+    return const NotAuthenticated();
   }
 }
---
+++ lib/screens/profile/local_widgets/not_authenticated.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:mangadex_mobile/routing/application.dart';
+import 'package:mangadex_mobile/widgets/icon_button.dart';
+
+class NotAuthenticated extends StatelessWidget {
+  const NotAuthenticated({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: Text(
+            "Oops! Looks like no user is signed in currently!",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        CustomIconButton(
+          text: 'Login',
+          icon: LineIcons.userShield,
+          onPressed: () => Application.router.navigateTo(context, "/login"),
+        ),
+        CustomIconButton(
+          text: 'Register',
+          icon: LineIcons.userPlus,
+          onPressed: () => Application.router.navigateTo(context, "/register"),
+        ),
+      ],
+    );
+  }
+}
