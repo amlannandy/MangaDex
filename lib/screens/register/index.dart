710ad9218eb8bdff0913cc510992e6a4395c8f39
amlannandy <amlannandy5@gmail.com>
2022-09-20 10:12:36 +0530

    implemented register functionality

--- lib/screens/register/index.dart
+++ lib/screens/register/index.dart
@@ -1,13 +1,54 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+import 'package:webview_flutter/webview_flutter.dart';
+
+import 'package:mangadex_mobile/routing/application.dart';
+import 'package:mangadex_mobile/utils/constants.dart';
+import 'package:mangadex_mobile/utils/snackbar.dart';
+
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
   @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Register'),
+    return Scaffold(
+      body: SafeArea(
+        child: WebView(
+          initialUrl: REGISTER_URL,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+          onPageFinished: (String url) {
+            if (url != REGISTER_URL) {
+              Application.router.navigateTo(context, '/login', replace: true);
+              showSnackbar(
+                context,
+                'Account created successfully! Please login',
+                ESnackBarType.success,
+              );
+            }
+          },
+        ),
       ),
     );
   }
